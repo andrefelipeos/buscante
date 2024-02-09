d0497df1f295aeb3a468f1b0bf07071bf5d5697d
André Felipe <andre.felipe.2048@gmail.com>
2024-02-09 13:44:31 -0300

    Map items returned by the service to LivroVolumeInfo objects

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
+import { Item, Livro } from 'src/app/models/interfaces';
 import { GoogleBooksService } from 'src/app/service/google-books.service';
 
 @Component({
@@ -9,7 +11,7 @@ import { GoogleBooksService } from 'src/app/service/google-books.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = "";
   subscription: Subscription;
 
@@ -19,17 +21,17 @@ export class ListaLivrosComponent implements OnDestroy {
     this.subscription = this.service
       .buscar(this.campoBusca)
       .subscribe({
-        next: retornoApi => console.log(retornoApi),
-        error: erroApi => console.error(erroApi),
-        complete: () => console.log("Observable completado."),
+        next: itens => this.listaLivros = this.livrosResultadoParaLivros(itens),
+        error: erroApi => console.error(erroApi)
       });
   }
 
+  livrosResultadoParaLivros(itens: Item[]): LivroVolumeInfo[] {
+    return itens.map(item => new LivroVolumeInfo(item));
+  }
+
   ngOnDestroy(): void {
-      this.subscription.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
 }
-
-
-
