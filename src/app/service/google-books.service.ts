5c133da534a7e57779b0c725a0a8b63c7ce883e9
André Felipe <andre.felipe.2048@gmail.com>
2024-02-09 13:41:19 -0300

    Reduce API response to a array of items / books

--- src/app/service/google-books.service.ts
+++ src/app/service/google-books.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LivrosResultado } from '../models/interfaces';
+import { Observable, map } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +12,10 @@ export class GoogleBooksService {
 
   constructor(private http: HttpClient) { }
 
-  buscar(termoDigitado: string): Observable<LivrosResultado> {
+  buscar(termoDigitado: string): Observable<Item[]> {
     const params: HttpParams = new HttpParams().append('q', termoDigitado);
     return this.http.get<LivrosResultado>(this.API, { params })
+      .pipe(map(payload => payload.items));
   }
 
 }
