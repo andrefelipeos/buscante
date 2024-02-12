d5cd62bdf64850f29ee32fb5114077d5ae8c5721
André Felipe <andre.felipe.2048@gmail.com>
2024-02-12 16:37:44 -0300

    Use debounceTime to delay search queries while typing

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter, map, switchMap } from 'rxjs';
+import { debounceTime, filter, map, switchMap } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
 import { GoogleBooksService } from 'src/app/service/google-books.service';
+
+const PAUSA = 600;
 
 @Component({
   selector: 'app-lista-livros',
@@ -16,6 +18,7 @@ export class ListaLivrosComponent {
   constructor(private service: GoogleBooksService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
     filter(termoDigitado => termoDigitado.length > 2),
     switchMap(termoDigitado => this.service.buscar(termoDigitado)),
     map(itens => itens.map(item => new LivroVolumeInfo(item)))
